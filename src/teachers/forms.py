3924b8b356785e092b7a7cb78fc0103416b6db23
ZenCrazyCat <zencrazycat@gmail.com>
2020-01-30 19:15:40 +0200

    add to hw11

--- src/teachers/forms.py
+++ src/teachers/forms.py
@@ -9,15 +9,15 @@ class TeachersAddForm(ModelForm):
         model = Teacher
         fields = '__all__'
 
-    def clean_email(self):
-        email = self.email
-        email_exists = Teacher.objects. \
-            filter(email__iexact=email). \
-            exclude(email__iexact=self.instance.email) \
-            .exists()
-        if email_exists:
-            raise ValidationError(f'{email} is already used!')
-        return email
+    # def clean_email(self):
+    #     email = self.instance.email
+    #     email_exists = Teacher.objects. \
+    #         filter(email__iexact=email). \
+    #         exclude(email__iexact=self.instance.email) \
+    #         .exists()
+    #     if email_exists:
+    #         raise ValidationError(f'{email} is already used!')
+    #     return email
 
 
 class TeacherAdminForm(TeachersAddForm):
