fe38145ec13544e336dc17982b8a59f6498cee7e
ZenCrazyCat <zencrazycat@gmail.com>
2020-02-17 11:46:42 +0200

    redo logger_cleaner

--- src/students/tasks.py
+++ src/students/tasks.py
@@ -1,17 +1,10 @@
-from time import sleep
+from datetime import timedelta
 
 from celery import shared_task
 from django.core.mail import send_mail
 from django.utils import timezone
 
 from students.models import Logger
-
-@shared_task
-def add(a, b):
-    print('ADD WORKS!')
-    sleep(10)
-    print(a + b)
-    return a + b
 
 
 @shared_task
@@ -22,7 +15,4 @@ def send_email_async(subject, message, email_from, recipient_list):
 @shared_task
 def logger_cleaner():
     now = timezone.now()
-    queryset = Logger.objects.all()
-    delete_list = (log for log in queryset if (now - log.created).days > 7)
-    for log in delete_list:
-        log.delete()
+    Logger.objects.filter(created__lte=now - timedelta(days=days)).delete()
